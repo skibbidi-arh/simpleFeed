a3bbf56c4450c61bab95f203d70fe016c8793e98
arhamapon153 <arhamapon@iut-dhaka.edu>
2026-07-10 02:08:54 +0600

    frontend folder

--- backend/app/app.py
+++ backend/app/app.py
@@ -29,7 +29,7 @@ app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix=
 async def upload_file(
         file: UploadFile = File(...),
         caption: str = Form(""),
-        user: User = Depends(curernt_active_user),
+        user: User = Depends(current_active_user),
         session: AsyncSession = Depends(get_async_session)
 ):
     temp_file_path = None
